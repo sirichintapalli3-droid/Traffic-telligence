00b621b4852f854b7611af88f8bf6fd6564ecbbd
SIRI <sirichintapalli3@gmail.com>
2026-05-22 11:39:59 +0530

    Delete App.py

--- App.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request
-import pickle
-import numpy as np
-import pandas as pd
-app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
